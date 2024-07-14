d35b7d3b13c0bc3c78d48aa11bd3f198161dd02c
Illia Dymura <illiadimura@gmail.com>
2024-07-14 12:07:13 +0300

    feat: breed checking: extract all breeds into breeds.json and use it instead of catapi calls

--- app/internal/repository/catapi/client.go
+++ app/internal/repository/catapi/client.go
@@ -2,79 +2,42 @@ package catapi
 
 import (
 	"context"
+	_ "embed"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"strings"
-
 	"github.com/illiafox/spy-cat-test-assignment/app/internal/apperrors"
 )
 
-const BaseURL = "https://api.thecatapi.com"
+//go:embed breeds.json
+var breedsFile []byte
 
-type Client struct{}
-
-func NewClient() *Client {
-	return &Client{}
+type Client struct {
+	Breeds map[string]struct{}
 }
 
-func (c Client) CheckBreed(ctx context.Context, breed string) (formattedBreed string, err error) {
-	path := "/v1/breeds/search?attach_image=0&q=" + url.QueryEscape(breed)
-	requestURL := BaseURL + path
+func NewClient() (*Client, error) {
+	var breeds []string
 
-	req, err := http.NewRequestWithContext(ctx, "GET", requestURL, nil)
+	err := json.Unmarshal(breedsFile, &breeds)
 	if err != nil {
-		return "", apperrors.Internal(err).Wrap("http: new request")
+		return nil, fmt.Errorf("unmarshall breeds from file: %w", err)
 	}
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", apperrors.Internal(err).Wrap("http: default client: do")
+	client := &Client{}
+
+	client.Breeds = make(map[string]struct{}, len(breeds))
+	for _, breed := range breeds {
+		client.Breeds[breed] = struct{}{}
 	}
 
-	defer func() {
-		_ = resp.Body.Close()
-	}()
+	return client, nil
+}
 
-	var body []struct {
-		Name string `json:"name"`
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		errBody, _ := io.ReadAll(resp.Body)
-		return "", apperrors.Internal(errors.New(string(errBody))).
-			Wrap("status code != 200").WithMetadata("status", resp.StatusCode)
-	}
-
-	if err = json.NewDecoder(resp.Body).Decode(&body); err != nil {
-		return "", apperrors.Internal(err).Wrap("json: unmarshall body")
-	}
-
-	if len(body) == 0 {
+func (c Client) CheckBreed(_ context.Context, breed string) (formattedBreed string, err error) {
+	_, ok := c.Breeds[breed]
+	if !ok {
 		return "", apperrors.InvalidCatBreed(breed, "not found")
 	}
 
-	if len(body) > 1 { // name is ambiguous
-		const limit = 5
-
-		otherBreeds := make([]string, 0, limit)
-		for i := 0; i < limit && i < len(body); i++ {
-			otherBreeds = append(otherBreeds, body[i].Name)
-		}
-
-		return "", apperrors.InvalidCatBreed(breed,
-			fmt.Sprintf("is ambiguous, other variants: %s", strings.Join(otherBreeds, ", ")),
-		)
-	}
-
-	if strings.ToLower(breed) != strings.ToLower(body[0].Name) {
-		return "", apperrors.InvalidCatBreed(breed,
-			fmt.Sprintf("maybe you meant '%s'?", body[0].Name),
-		)
-	}
-
-	return body[0].Name, nil
+	return breed, nil
 }
